993f3f39c2ddaa474a06af19aa44a64b1c5e48fa
Daniel Carrera <dcarrera@gmail.com>
2012-08-11 17:30:09 +0200

    * Rewrite the plotting API. The new API is incompatible with version 0.0.1   but it is more powerfl and easier to use.
    * Replace the examples in the README with a proper tutorial.
    
    * Add a ChangeLog and Roadmap.

--- lib/bplot.rb
+++ lib/bplot.rb
@@ -1,89 +1,4 @@
 =begin
-BPlot -- A ploting module for SciRuby. It provides an API
-         familiar to Gnuplot users and to a lesser extent
-         Matlab users.
-
-                 \     /
-             \    o ^ o    /
-               \ (     ) /
-    ____________(%%%%%%%)____________
-   (     /   /  )%%%%%%%(  \   \     )
-   (___/___/__/           \__\___\___)
-      (     /  /(%%%%%%%)\  \     )
-       (__/___/ (%%%%%%%) \___\__)      A hard-working bee.
-               /(       )\
-             /   (%%%%%)   \
-                  (%%%)
-                    !
-
-EXAMPLES: 2D PLOTS
--------------------
-
-require 'bplot'
-
-x = [1,2,3,4,5]
-y = [1,4,9,16,25]
-z = [25,20,15,10,5]
-
-b = BPlot.new
-
-#
-# Send a raw command into Gnuplot.
-#
-b.cmd('plot sin(x) title "Plot without Ruby."')
-
-
-#
-# Gnuplot 'set' command.
-#
-b.set('xrange [0:6]')
-b.set('yrange [0:30]')
-b.set('title "Heading for the entire plot"')
-
-
-#
-# Gnuplot 'replot' command.
-#
-b.replot
-
-
-#
-# Basic plots.
-#
-b.plot(x, y)
-b.plot(x, y, 'ps 2', 'rh')
-b.plot(x, y, 'ps 2', 'rh--', 'y = x^2')
-
-#
-# The 'with' command on the gnuplot string takes precedence.
-#
-b.plot(x, y, 'ps 2 w lp', 'rh')
-b.plot(x, y, 'ps 2 with steps', 'rh')
-
-
-#
-# Named parameters.
-#
-b.plot(x, y, :title => 'Density', :gnuplot => 'lc 3 ps 1.5 lt 4 pt 5 w lp')
-b.plot(x, y, :title => 'Density', :gnuplot => 'ps 1.5', :matlab => 'rh-')
-b.plot(x, y, :t => 'Mass', :g => 'ps 2.5', :m => 'bh--')
-
-
-#
-# Multiple data sets (need not be the same length).
-#
-b.plot(x, y, 'ps 2', 'rh-', x, z, 'ps 1.5', 'bs--')
-b.plot(x, y, 'ps 2', 'rh-', 'Quadratic', x, z, 'ps 1.5', 'bs--', 'Linear')
-
-
-# Observe that named parameters must be at the end. Hence, they only apply to the last plot.
-
-b.plot(x, y, 'ps 2', 'rh-',       'Quadratic', x, z, 'ps 1.5', 'bs--', :t => 'Linear') # OK.
-b.plot(x, y, 'ps 2', 'rh-', :t => 'Quadratic', x, z, 'ps 1.5', 'bs--',       'Linear') # ERROR.
-
-
-
-========================================
 TODO: SURFACE PLOTS
 -------------------
 
@@ -101,6 +16,7 @@ b.splot(x,y,z)
 #
 # I'll document this function when I implement surface plots.
 #
+=begin
 def meshgrid(x,y)
 	warn "Input to meshgrid() must be 1 dimensional" if x.dim > 1
 	warn "Input to meshgrid() must be 1 dimensional" if y.dim > 1
@@ -113,8 +29,26 @@ def meshgrid(x,y)
 	
 	return xx, yy.transpose
 end
+=end
 
+
+# BPlot is a 2D and 3D plotting module for SciRuby. It provides a
+# simple API that will be familiar to Gnuplot users and to a lesser
+# extent Matlab users. BPlot is in ALPHA status. Two dimmensional
+# plots seem to work well but they really need more testing. Also,
+# the API is not yet settled and may change without warning. Many
+# features are not yet implemented, including surface plots.
 class BPlot
+	# === Create new engine
+	# 
+	# Every call to BPlot#new creates a new instance of Gnuplot. You can
+	# have multiple instances of Gnuplot in the same Ruby script.
+	#
+	# === Example
+	#
+	#   b.BPlot.new
+	#   b.cmd('plot sin(x)')
+	# 
 	def initialize()
 		#
 		# Every instance of BPlot has its own gnuplot process.
@@ -130,30 +64,102 @@ class BPlot
 	end
 	
 	#
-	# Command -- Send a raw command into Gnuplot.
-	#            Gives the user more control over Gnuplot.
+	# === Issue raw command
+	# 
+	# Send a raw command to the Gnuplot backend.
+	# This gives the user more control over plotting engine.
+	#
+	# === Example
+	#
+	#   b.BPlot.new
+	#   b.cmd('plot sin(x)')
 	#
 	def cmd(str)
 		@pipe.puts str
 	end
 	
 	#
-	# Settings -- Wrapper for 'set' command in Gnuplot.
+	# === Settings
+	# 
+	# This method is a thin wrapper around the Gnuplot "set" command.
+	# It can be used to set *a lot* of options. However, nothing is
+	# drawn until the user issues a plotting command. See the examples
+	# below for a quick overview. See the Gnuplot documentation for
+	# more details.
+	#
+	# === Examples
+	#
+	#   b.set('terminal enhanced postscript color')
+	#   b.set('output "myplot.ps"')
+	#   b.set('xrange [0:10]')
+	#   b.set('yrange [0:50]')
+	#   b.set('xlabel "This is the X Axis"')
+	#   b.set('ylabel "This is the Y Axis"')
+	#   b.set('title "A title for the whole plot"')
 	#
 	def set(str)
 		@pipe.puts "set #{str}"
 	end
 	
-	#
-	# Replot -- Wrapper for the 'replot' command in Gnuplot.
-	#
-	def replot
-		@pipe.puts "replot"
+	# === Settings
+	# 
+	# This method is a thin wrapper around the Gnuplot "unset" command.
+	# It is used to reset settings back to their default values. See
+	# the documentation on "set" for more information. See also the
+	# Gnuplot documentaion for "set" and "unset".
+	def unset(str)
+		@pipe.puts "unset #{str}"
 	end
 	
 	#
-	# 2D plotting -- Mostly like Gnuplot, but adding colours and line/point styles from Matlab.
+	# === Refresh
 	#
+	# This command reformats and redraws the current plot with the latest
+	# settings. This is useful for viewing a plot with different "set"
+	# options, or for generating the same plot for several output formats.
+	#
+	# === Example
+	#
+	#   b.set('terminal postscript')
+	#   b.set('output "myplot.ps"')
+	#   b.refresh
+	#   
+	#   b.set('terminal png')
+	#   b.set('output "myplot.png"')
+	#   b.refresh
+	#
+	def refresh()
+		@pipe.puts "refresh"
+	end
+	
+	# == Multiplot
+	# 
+	# In multiplot mode, multiple plot commands are placed togeher in the
+	# same window. This can be a convenient alternative to having a very
+	# long multiple-line plot command. It allso allows you to add plots
+	# using loops and other constructions.
+	# 
+	# == Examples
+	#
+	#   b.multiplot do
+	#     plot(x, y1)
+	#     plot(x, y2)
+	#     plot(x, y3)
+	#   end
+	#   
+	#   b.multiplot do
+	#     (0..10).each { |t| plot( x, sin(x + v*t) ) }
+	#   end
+	def multiplot(opts='')
+		@pipe.puts "set multiplot #{opts}"
+		yield if block_given?
+		@pipe.puts "unset multiplot"
+	end
+	
+	# == 2D Plotting
+	# 
+	# This is the key method in BPlot, as it is responsible for all 2D plots. This
+	# method is currently in a state of flux and is for the moment undocumented.
 	def plot(*args)
 		
 		data = []
@@ -203,106 +209,115 @@ class BPlot
 	private
 	
 	#
-	# TODO: Can I avoid using strings here?
-	#
-	def finished(args)
-		args.length == 0 or
-					args[0].class.to_s == 'Array' or
-					args[0].class.to_s == 'NMatrix'
-	end
-	
-	#
 	# - Get all the stye information for the next plot from 'args'.
 	# - Return the styles as a string, and the remainder of 'args'.
 	#
 	def styles(args)
 		
-		g_with = ''
-		m_with = ''
-		str_index = 0
-		this_plot = " '-' "
-		while !finished(args)
-			if args[0].is_a?(String)
+		with  = ''
+		title = ''
+		color = ''
+		style = " '-' "
+		
+		if args[0].is_a?(String)
+			
+			opts = args.shift
+			opts = opts.split(/;/)
+			
+			#
+			# Gnuplot 'with' beats Matlab's.
+			#
+			with = gnu_with(opts[2]) if opts[2]
+			with = mat_with(opts[0]) if with == ''
+			
+			style << mat_style(opts[0])
+			style << gnu_style(opts[2]) if opts[2]
+			
+			title = opts[1] if opts.length >= 1
+		end
+		
+		if args[0].is_a?(Hash)
+			#
+			# Incidentally, if we are here, this is the end of the plot command.
+			#
+			opts = args.shift
+			
+			title  = opts[:title]  if opts[:title]
+			color  = opts[:color]  if opts[:color]
+			
+			#
+			# Infer the 'with' command for :lines and :points.
+			#
+			if opts[:lines]
+				lw, lt = opts[:lines].split(/;/)
 				
-				g_with << gnu_with(args[0]) 		if str_index == 0
-				m_with << mat_with(args[0]) 		if str_index == 1
+				lw = 1  if lw == ''
+				lt = 1  if lt == '' or lt == nil
+				lt = 1  if lt.to_s =~ /solid/i
+				lt = 2  if lt.to_s =~ /dashed/i
+				lt = 3  if lt.to_s =~ /dots/i
+				lt = 4  if lt.to_s =~ /dot-dashed/i
+				lt = 5  if lt.to_s =~ /dot-dot-dashed/i
 				
-				this_plot << gnu_style(args.shift)	if str_index == 0
-				this_plot << mat_style(args.shift)	if str_index == 1
-				this_plot << tit_style(args.shift)	if str_index == 2
+				style << " lw #{lw} lt #{lt} "
+			end
+			if opts[:points]
+				ps, pt = opts[:points].split(/;/)
 				
-				str_index += 1
+				ps = 1  if ps == ''
+				pt = 1  if pt == ''
+				pt = 1  if pt == '+'
+				pt = 2  if pt == 'x' or pt == 'X'
+				pt = 3  if pt == '*'
+				pt = 1  if pt.to_s =~ /plus/i
+				pt = 2  if pt.to_s =~ /cross/i
+				pt = 3  if pt.to_s =~ /star/i
+				pt = 4  if pt.to_s =~ /open-square/i
+				pt = 5  if pt.to_s =~ /solid-square/i
+				pt = 6  if pt.to_s =~ /open-circle/i
+				pt = 7  if pt.to_s =~ /solid-circle/i
+				pt = 8  if pt.to_s =~ /open-up-triangle/i
+				pt = 9  if pt.to_s =~ /solid-up-triangle/i
+				pt = 10 if pt.to_s =~ /open-down-triangle/i
+				pt = 11 if pt.to_s =~ /solid-down-triangle/i
+				pt = 12 if pt.to_s =~ /open-diamond/i
+				pt = 13 if pt.to_s =~ /solid-diamond/i
 				
-			elsif args[0].is_a?(Hash)
-				opts = args.shift
-				
-				# Gnuplot style.
-				g_with << gnu_with(opts[:g])			if opts[:g]
-				g_with << gnu_with(opts[:gn])			if opts[:gn]
-				g_with << gnu_with(opts[:gnu])			if opts[:gnu]
-				g_with << gnu_with(opts[:gnup])			if opts[:gnup]
-				g_with << gnu_with(opts[:gnupl])		if opts[:gnupl]
-				g_with << gnu_with(opts[:gnuplo])		if opts[:gnuplo]
-				g_with << gnu_with(opts[:gnuplot])		if opts[:gnuplot]
-				
-				this_plot << gnu_style(opts[:g])		if opts[:g]
-				this_plot << gnu_style(opts[:gn])		if opts[:gn]
-				this_plot << gnu_style(opts[:gnu])		if opts[:gnu]
-				this_plot << gnu_style(opts[:gnup])		if opts[:gnup]
-				this_plot << gnu_style(opts[:gnupl])	if opts[:gnupl]
-				this_plot << gnu_style(opts[:gnuplo])	if opts[:gnuplo]
-				this_plot << gnu_style(opts[:gnuplot])	if opts[:gnuplot]
-				
-				
-				# Matlab style.
-				m_with << mat_with(opts[:m])			if opts[:m]
-				m_with << mat_with(opts[:ma])			if opts[:ma]
-				m_with << mat_with(opts[:mat])			if opts[:mat]
-				m_with << mat_with(opts[:matl])			if opts[:matl]
-				m_with << mat_with(opts[:matla])		if opts[:matla]
-				m_with << mat_with(opts[:matlab])		if opts[:matlab]
-				
-				this_plot << mat_style(opts[:m])		if opts[:m]
-				this_plot << mat_style(opts[:ma])		if opts[:ma]
-				this_plot << mat_style(opts[:mat])		if opts[:mat]
-				this_plot << mat_style(opts[:matl])		if opts[:matl]
-				this_plot << mat_style(opts[:matla])	if opts[:matla]
-				this_plot << mat_style(opts[:matlab])	if opts[:matlab]
-				
-				# Title.
-				this_plot << tit_style(opts[:t])		if opts[:t]
-				this_plot << tit_style(opts[:ti])		if opts[:ti]
-				this_plot << tit_style(opts[:tit])		if opts[:tit]
-				this_plot << tit_style(opts[:titl])		if opts[:titl]
-				this_plot << tit_style(opts[:title])	if opts[:title]
+				style << " ps #{ps} pt #{pt} "
 			end
 			
+			#
+			# "with" strings from both Matlab and Gnuplot take precedence.
+			#
+			# In other words:  Gnuplot > Matlab > Named parameters. 
+			#
+			if with == ''
+				with = 'with lines'       if opts[:lines]
+				with = 'with points'      if opts[:points]
+				with = 'with linespoints' if opts[:lines] and opts[:points]
+			end
 		end
 		
 		
-		# - The user may insert a 'with' in the gnuplot string which conflicts with matlab.
-		# - In this case, the gnuplot string takes precdence.
-		# - If you insert the word "with" in a title inside the gnuplot string... it's your
-		#   own fault. I told you to put the title in the :title parameter.
+		#
+		# Finish up the command (title, colour, 'with', etc).
+		#
+		style << " lc rgb \"#{color}\" " if color != ''
+		style << " title  \"#{title}\" "
+		style << with if with
 		
-		this_plot << g_with
-		this_plot << m_with if g_with == ''
-		
-		#
-		# If there is no title, add a blank one.
-		#
-		this_plot << " title '' " if this_plot !~ /tit/
-		
-		#
-		# Return 'this_plot' and what is left of 'args'.
-		#
-		return this_plot, args
+		return style, args
 	end
 	
 	
 	def gnu_with(s)
-		s =~ /w/ ? s.scan(/([wW][iI]?[tT]?[hH]? +\w+)/).last.first : ''
+		regex = /\b([wW][iI]?[tT]?[hH]? +\w+)/
+		s =~ regex ? s.scan(regex).last.first : ''
 	end
+	def gnu_style(str)
+		str.gsub(/\b([wW][iI]?[tT]?[hH]? +\w+)/, '')
+	end
+	
 	def mat_with(s)
 		wp = s =~ /[xsovdph<>+*^]/ ? true : false
 		wl = s =~ /[-:.]/ ? true : false
@@ -310,13 +325,6 @@ class BPlot
 		return " with linespoints " if wp and wl
 		return " with points "		if wp
 		return " with lines "       if wl
-	end
-	
-	def tit_style(str)
-		" title \"#{str}\" "
-	end
-	def gnu_style(str)
-		str.gsub(/([wW][iI]?[tT]?[hH]? +\w+)/, '')
 	end
 	def mat_style(mat)
 		style = '';
@@ -341,10 +349,15 @@ class BPlot
 		style << " pt 13 " if mat =~ /d/
 		
 		# Styles I cannot duplicate properly:
-		style << " pt 8  " if mat =~ /</
-		style << " pt 10 " if mat =~ />/
-		style << " pt 4  " if mat =~ /p/
-		style << " pt 7  " if mat =~ /h/
+		style << " pt 8  " if mat =~ /</  # open up triangle.
+		style << " pt 10 " if mat =~ />/  # open down triangle.
+		style << " pt 12 " if mat =~ /p/  # open diamond
+		style << " pt 7  " if mat =~ /h/  # solid circle
+		
+		#
+		# Extra remaining style (non-Matlab).
+		#
+		style << " pt 4  " if mat =~ /q/  # open square
 		
 		# Line styles
 		style << " lt 1 " if mat =~ /-/ and mat !~ /--/ and mat !~ /\.-/ and mat !~ /\.\.-/
